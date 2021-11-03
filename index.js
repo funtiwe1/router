b16f9e1a63a55eb64e157fd34247302691a04829
alex <alex@informunity.ru>
2021-11-03 12:56:32 +0300

    auto

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const client = require('ari-client');
 const fs = require('fs');
 const sleep = require('./utils.js').sleep
 const Log = require('./utils.js').Log
-const udpserver = require('./udpserver')
 const asr = require('./speech').asr_s
 const tts = require('./speech').tts_f
 const getRTP = require('./utils').getRTP
+const IVR = require('./ivr').IVR
 
 const IP_RTPSERVER = '5.189.230.61';
 const IP_ASTERSERVER = '5.189.230.61';
@@ -19,6 +19,10 @@ const APPNAME = 'router';
 client.connect('http:\/\/' + IP_ASTERSERVER + ':8088', 'amd', '57d5cf235bc84181cb101335ce689eba',function (e, ari) {
   //client.connect('http:\/\/' + IP_ASTERSERVER + ':8088', 'amd', '57d5cf235bc84181cb101335ce689eba',function (err, ari) {
   let log = new Log('router.log');
+  let curport = 8010;
+  let text = 'Вы находитесь в главном меню. Для выбора приложения введите цифру. Один - повторялка. Два - АМД!';
+  let ivr = null;
+
   if (e) {
     log.log('Error connect asterisk ari')
     log.log(e.message);
@@ -29,85 +33,35 @@ client.connect('http:\/\/' + IP_ASTERSERVER + ':8088', 'amd', '57d5cf235bc84181c
   ari.start(APPNAME);
   log.log('Started '+APPNAME+' app');
 
-  //
-
-  let curport = 8010;
-  //let external_host = IP_RTPSERVER + ':' + port;
-
   ari.once('StasisStart', async (event, outgoing)=> {
     log.log('StasisStart %s',outgoing.id);
-    let rs = null;
-    let usrv = null;
-    //let text = 'Вы находитесь в главном меню. Для выбора приложения введите цифру. Один - повторялка. Два - АМД!';
-    let text = 'Вы!';
-    let filename = 'del_'+new Date().getTime();
-    let port = curport++;
-    let rtext = '';
-
-    // play(text,ari,outgoing)
-    // .then(record())
-
     try {
-      filename = await tts(text,filename,'\/var/spool\/asterisk\/recording\/');
-    } catch(e){
-      log.log('Error get tts');
-      throw new Error(e.message);
+      ivr = new IVR(ari,curport++,outgoing,APPNAME,IP_RTPSERVER);
+    } catch(e) {
+      log.log('Error create IVR '+e.message);
+      return;
     }
 
-    let playback = new ari.Playback();
-    outgoing.play({media:'recording:'+filename},playback)
-    .then(async ()=>{
-      //record();
-      playback.on('PlaybackFinished',async ()=>{
-        record();
-      });
-      log.log('Started play');
-    }).catch((e)=>{
-      throw new Error(e.message);
-    });
-
-    function record() {
-      log.log('Finished play');
-
-      getRTP(ari,APPNAME,IP_RTPSERVER,port,outgoing)
+    ivr.play(text)
+    .then((d)=>{
+      ivr.record(2000)
       .then((d)=>{
-        try {
-          rs = asr();
-          usrv = new udpserver.RtpUdpServerSocket(IP_RTPSERVER + ':' + port,rs);
-
-//          console.log(rs)
-          rs.on('data',(d)=>{
-            rtext = d.results[0].alternatives[0].transcript;
-            //log.log(rtext);
-          });
-
-          setTimeout(()=>{
-            log.log(rtext)
-            if (!rtext) return;
-            let r = rtext;
-
-            usrv.close();
-            rs.end();
-            rtext = '';
-            switch (r) {
-              case 'Повторялка':outgoing.move({app:'ivr'});break;
-              case 'Автоответчики': outgoing.move({app:'amd'});break;
-              default:{
-                //let playback = new ari.Playback();
-                outgoing.move({app:'amd'}).catch((e)=>{log.log(e)});
-                //outgoing.play({media:'recording:'+filename},playback);
-              };
-            }
-          },2000);
-        } catch(e) {
-          log.log('Error get asr');
-          throw new Error(e.message);
+        log.log(d);
+        switch (d) {
+          case 'Повторялка':outgoing.move({app:'ivr'});break;
+          case 'Автоответчики': outgoing.move({app:'amd'});break;
+          default:{
+            //let playback = new ari.Playback();
+            outgoing.move({app:'amd'}).catch((e)=>{log.log(e)});
+            //outgoing.play({media:'recording:'+filename},playback);
+          };
         }
-      });
-    }
+      })
+    })
 
     outgoing.once('StasisEnd', function (event, ch) {
       log.log('StasisEnd: '+ch.id);
+      ivr.close();
     });
 
     outgoing.once('ChannelHangupRequest', (e,ch)=>{
---
+++ ivr.js
@@ -0,0 +1,110 @@
+'use strict'
+const udpserver = require('./udpserver')
+const speech = require('@google-cloud/speech')
+const Log = require('./utils.js').Log
+const asr = require('./speech').asr_s
+const tts = require('./speech').tts_f
+const getRTP = require('./utils').getRTP
+
+
+const encoding = 'LINEAR16';
+const sampleRateHertz = 16000;
+const languageCode = 'ru-RU';
+
+// google request
+const request_g = {
+  config: {
+    encoding: encoding,
+    sampleRateHertz: sampleRateHertz,
+    languageCode: languageCode,
+  },
+  interimResults: true, // If you want interim results, set this to true
+};
+
+class IVR {
+  rs = null;
+  usrv = null;
+  ari = null;
+  port = null;
+  ch = null;
+  appname = null;
+  rtpserver = null;
+  last_text = '';
+  client = null;
+  rs = null;
+
+  constructor(ari,port,ch,appname,rtpserver) {
+    this.ari = ari;
+    this.port = port;
+    this.ch = ch;
+    this.appname = appname;
+    this.rtpserver = rtpserver;
+
+    this.client = new speech.SpeechClient();
+    this.rs = this.client
+    .streamingRecognize(request_g)
+    .on('error', console.error)
+  } catch(e) {
+    return new Error(e.message)
+
+    this.usrv = new udpserver.RtpUdpServerSocket(this.rtpserver + ':' + this.port,this.rs);
+  }
+
+  play(text) {
+    return new Promise(async (res,rej)=>{
+      let   log = new Log('router.log');
+
+    let filename = 'del_'+new Date().getTime();
+    try {
+      filename = await tts(text,filename,'\/var/spool\/asterisk\/recording\/');
+    } catch(e){
+      log.log('Error get tts');
+      throw new Error(e.message);
+    }
+
+    let playback = new this.ari.Playback();
+    this.ch.play({media:'recording:'+filename},playback)
+    .then(async ()=>{
+      //record();
+      playback.on('PlaybackFinished',async ()=>{
+        log.log('Finished play');
+        res();
+      });
+      log.log('Started play');
+    }).catch((e)=>{
+      throw new Error(e.message);
+    });
+
+  })
+}
+
+record(time){
+  return new Promise((res,rej)=>{
+    function record() {
+      log.log('Start record');
+      getRTP(this.ari,this.appname,this.rtpserver,this.port,this.ch)
+      .then((d)=>{
+        try {
+          //rs = asr();
+          this.rs.on('data',(d)=>{
+            this.last_text = d.results[0].alternatives[0].transcript;
+            //log.log(rtext);
+          });
+
+          setTimeout(()=>{
+            //log.log(this.last_text);
+            usrv.close();
+            rs.end();
+            if (!this.last_text) res(null);
+            else res(this.last_text);
+          },time);
+        } catch(e) {
+          log.log('Error get asr');
+          throw new Error(e.message);
+        }
+      });
+    }
+  })
+}
+}
+module.exports.IVR = IVR;
